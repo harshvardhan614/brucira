0cc5d8a94eb9f56487979587d0e7861995a212ad
Harshvardha Rathore <rharshvardhan614@gmail.com>
2024-08-01 13:00:23 +0530

    commit change

--- components/MainHead.tsx
+++ components/MainHead.tsx
@@ -8,7 +8,7 @@ const MainHead = () => {
   return (
     <main className="flexBetween min-h-screen flex-col overflow-clip">
       <div className="w-full relative">
-        <Image src="/heroBg.svg" alt="" className="absolute left-0 top-0 right-0 h-full md:h-auto max-h-[800px] w-full object-cover -z-10" width={1320} height={720} />
+        <Image src="/herobg.svg" alt="" className="absolute left-0 top-0 right-0 h-full md:h-auto max-h-[800px] w-full object-cover -z-10" width={1320} height={720} />
         <Navbar/>
         <Header />
       </div>     
